760385dc88462a82e237af330f6e89348df10e45
Andrey Nazarov <andreynazarov3@yandex.ru>
2018-03-11 23:58:03 +0400

    rename to php

--- index.php
+++ index.php
@@ -1,1 +1,60 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="assets/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Mina:400,700" rel="stylesheet">
+</head>
+<body>
+  <div class="wrapper">
+    <header>
+      <div class="logo">
+        <img src="assets/images/Koala-Cool-Logo-Circle.svg" alt="Logo">
+      </div>
+      <div class="name-wrapper">
+        <div class="name">
+          Andrey Nazarov
+        </div>
+        <div class="subname">
+          Cool guy with guns and playstation
+        </div>
+      </div>
+    </header>
+    <div class="line"></div>
+    <img src="assets/images/Surfing-Beach-High-Quality-Wallpaper.jpg" alt="Beach" class="big-image">
+    <div class="featured">
+      <h2 class="featured-heading">
+        Featured Work
+      </h2>
+      <div class="featured-items-wrapper-wrapper">
+        <div class="featured-items-wrapper">
+          <div class="featured-item">
+            <div class="featured-image-wrapper">
+            <img src="assets/images/united-states-america-map-com.jpg" alt="Map">
+          </div>
+            <h3 class="featured-item-name">Map</h3>
+            <a href="https://github.com/andreynazarov3/googlemap-with-firebase" class="featured-item-link">googlemap-with-firebase</a>
+          </div>
+          <div class="featured-item">
+              <div class="featured-image-wrapper">
+            <img src="assets/images/flex-banner.jpg" alt="">
+            </div>
+            <h3 class="featured-item-name">Flexbox Mostly Fluid</h3>
+            <a href="https://github.com/andreynazarov3/udacity-mostly-fluid" class="featured-item-link">udacity-mostly-fluid</a>
+          </div>
+          <div class="featured-item">
+              <div class="featured-image-wrapper">
+            <img src="assets/images/23541031-animal-card-collection-baby-tiger-happy-birthday.jpg" alt="">
+            </div>
+            <h3 class="featured-item-name">Animal Card</h3>
+            <a href="https://github.com/andreynazarov3/udacity-trading-card" class="featured-item-link">udacity-trading-card</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
